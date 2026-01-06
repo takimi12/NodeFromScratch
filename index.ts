b0d395ffe08ce86d3073173ed397592a15290c16
takimi12 <tomek12olech@gmail.com>
2026-01-06 22:52:11 +0100

    feat(chapter2): implement Pug template engine

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 
-
 import apiV1 from "./routes/v1";
 import pages from "./routes/pages";
 import bodyParser from "body-parser";
@@ -12,17 +11,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views")); 
+// Konfiguracja Pug
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/pages", pages);
 app.use("/api/v1", apiV1);
-
-
-
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Not found</h1>");
